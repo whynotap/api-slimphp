8bf1575ce6bdcdcaa8314a6b9486e13e86c44d6a
Ayato <146425893+whynotap@users.noreply.github.com>
2024-01-30 16:15:44 +0100

    Update index.php
    Ajout de la route "/login"

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 require 'vendor/autoload.php';
+require 'src/User.php'; // Assurez-vous que le chemin vers User.php est correct
 
-$factory = new \Nyholm\Psr7\Factory\Psr17Factory();
-$serverRequestCreator = new \Nyholm\Psr7Server\ServerRequestCreator($factory, $factory, $factory, $factory);
+use Slim\Factory\AppFactory;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
+use App\User;
+
+$factory = new Psr17Factory();
+$serverRequestCreator = new ServerRequestCreator($factory, $factory, $factory, $factory);
 $request = $serverRequestCreator->fromGlobals();
 
-$app = \Slim\Factory\AppFactory::create();
+$app = AppFactory::create();
 
 // Ajout de la configuration de la base de données
 $container = $app->getContainer();
@@ -21,6 +27,20 @@ $container['db'] = function ($c) {
 
 $app->get('/', function ($request, $response, $args) {
     $response->getBody()->write("Bienvenue sur SlimPHP !");
+    return $response;
+});
+
+$app->post('/login', function ($request, $response, $args) use ($container) {
+    $data = $request->getParsedBody();
+    $user = User::findByUsername($container['db'], $data['username']);
+
+    if ($user && password_verify($data['password'], $user->getPassword())) {
+        $_SESSION['user'] = $user->getId();
+        $response->getBody()->write('Vous êtes désormais connecté avec succès !');
+    } else {
+        $response->getBody()->write('Vos identifiants sont incorrect');
+    }
+
     return $response;
 });
 
