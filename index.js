8b123a0ab83c605d00c5b2e42ef234ee12353387
Python <146425893+whynotap@users.noreply.github.com>
2024-02-04 03:37:49 +0100

    Remove RegisterRouter

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const userRoutes = require('./routes/UserRoutes');
 const loginRouter = require('./routes/LoginRouter');
-const registerRouter = require('./routes/RegisterRouter');
 
 require('./database/db');
 const app = express();
 const port = 3000;
 
-app.use(express.json()); 
-app.use('/login', loginRouter);
-
-app.use(express.json()); 
-app.use('/register', registerRouter);
-
 app.use(express.json());
+
+app.use('/login', loginRouter);
 app.use('/users', userRoutes);
 
 app.listen(port, () => {
