fdbe86297d04f07d054c7b64604cbea0592a438b
Ayato <146425893+whynotap@users.noreply.github.com>
2024-02-04 23:11:07 +0100

    Update CatalogRouter.js
    Ajout de la route "catalog, catalog_items & items_base"

--- routes/CatalogRouter.js
+++ routes/CatalogRouter.js
@@ -13,4 +13,25 @@ router.get('/get/catalog', (req, res) => {
   });
 });
 
+router.get('/get/catalogitems', (req, res) => {
+    const query = 'SELECT id, item_ids, page_id, catalog_name, cost_credits, cost_credits, cost_points, points_type, amount, limited_stack, limited_sells, order_number, offer_id, song_id, extradata, have_offer, club_only, badge, rate FROM catalog_items';
+    db.query(query, (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+  router.get('/get/items', (req, res) => {
+    const query = 'SELECT id, sprite_id, public_name, item_name, type, width, length, stack_height, allow_stack, allow_sit, allow_lay, allow_walk, allow_gift, allow_trade, allow_recycle, allow_marketplace_sell, allow_inventory_stack, interaction_type, interaction_modes_count, vending_ids, multiheight, customparams, effect_id_male, effect_id_female, clothing_on_walk  FROM items_base';
+    db.query(query, (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
 module.exports = router;
