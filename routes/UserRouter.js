bd63282e501ac2b6b2e90ebbeae553007a7553e9
Ayato <146425893+whynotap@users.noreply.github.com>
2024-02-04 22:37:45 +0100

    Update UserRouter.js
    Ajout de la route "users-settings" lié a la table sql "users_settings"

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -18,6 +18,17 @@ router.get('/get/users', (req, res) => {
   });
 });
 
+router.get('/get/users-settings', (req, res) => {
+  const query = 'SELECT id, user_id, achievement_score, daily_respect_points, daily_pet_respect_points, respects_given, respects_received, guild_id, can_change_name, can_trade, is_citizen, citizen_level, helper_level, tradelock_amount, cfh_send, cfh_abusive, cfh_warnings, cfh_bans, block_following, block_friendrequests, block_roominvites, volume_system, volume_furni, volume_trax, old_chat, block_camera_follow, chat_color, home_room, online_time, tags, club_expire_timestamp, login_streak, rent_space_id, rent_space_endtime, hof_points, block_alerts, talent_track_citizenship_level, talent_track_helpers_level, ignore_bots, ignore_pets, nux, mute_end_timestamp, allow_name_change, perk_trade, forums_post_count, ui_flags, has_gotten_default_saved_searches, hc_gifts_claimed, last_hc_payday, max_rooms, max_friends, blockmentions, streak_last, streak_days, streak_level, mod_bubble, shadowban, mentions_enabled, daily_points  FROM users_settings';
+  db.query(query, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
 router.get('/get/user', (req, res) => {
   const { user_id } = req.body;
 
